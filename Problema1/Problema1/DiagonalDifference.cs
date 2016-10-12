4e4586fbc0133de7a2ab8d497821ff5b9b5b5f16
Darkichigo09 <jhuesitosacuna@gmail.com>
2016-10-12 13:47:42 -0600

    Ejercicio 1 y 2 y la Pagina en HTML Se adjuntan para la revsion

---
+++ Problema1/Problema1/DiagonalDifference.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema1
+{
+    class DiagonalDifference
+    {
+        static void Main(string[] args)
+        {
+            DiagonalDifference difference = new DiagonalDifference();
+            string matriz = "11 2 4|4 5 6|10 8 -12";
+            int aux = difference.matrix(matriz);
+            Console.WriteLine("   ");
+            Console.ReadKey();
+        }
+
+        public int matrix(string matriz)
+        {
+            matriz = "11 2 4|4 5 6|10 8 -12";
+            string[] split = matriz.Split(new Char[] { '|', ' ','\t' });
+            int raiz = Convert.ToInt32(Math.Sqrt(split.Length));
+            int diagonalMayor = 0;
+            int diagonalMenor = 0;
+            
+                  foreach (string s in split)
+               {
+
+                if (s.Trim() != " ") ;
+                       
+               }
+            Console.WriteLine("El string es el siguiente: " + matriz);
+
+
+            for (int i = 0; i < raiz; i++)//solo se necesita un ciclo ya que es un vector que se muestre como matriz pero en realidad solo se necesita un ciclo
+            {
+
+                diagonalMayor += Convert.ToInt32(split[(i * raiz) + i]);//variable donde el ciclo va a recorrer el y donde se cumpla la operacion y hace que almacene en la variable 
+                //el elemento que haya en la posicion lo multiplica con el numero de la raiz que es el numero que salga dependiendo del tamano del vector
+                diagonalMenor += Convert.ToInt32(split[(raiz * (i + 1) - 1) - i]);//iteracion similar a la anterior pero alrevez por eso realiza para obtener los elementos de la diagonal menor
+                //Console.WriteLine("La diagonal Mayor es: " + split[(i * raiz) + i]);
+                //Console.WriteLine("La diagonal Menor es: " + split[(raiz * (i + 1) - 1) - i]);
+            }
+            Console.WriteLine("La suma de la diagonal Mayor es: "+diagonalMayor);
+            Console.WriteLine("La suma de la diagonal Menor es: " + diagonalMenor);
+            Console.WriteLine("La resta de Diagonales es: " + (diagonalMayor - diagonalMenor));
+            return diagonalMayor - diagonalMenor;
+            //este codigo es optimo ya que solo realiza una funcion, no utiliza listas que lo harian mas confusos de lo normal, 
+            //utiliza un ciclo para recorrer el vector y utiliza las mismas librerias que vienen por defecto al cargar el codigo.
+            //Fue la solucion mas completa que pense y la que se me ocurrio realizar para resolver este problema.
+
+        }
+    }
+
+}
---
+++ Problema2/Problema2/CamelCase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema2
+{
+   public class CamelCase
+    {
+        static void Main(string[] args)
+        {//clase main donde se llama la clase calculate que contiene la funcion
+            calculate calculate = new calculate();
+            string prueba = "thisIsATestAndIHaveToProbeThatIsCorrectlyAndFunctioningGood";//string the prueba
+            Console.WriteLine("La cadena original es: "+prueba);//imprime la cadena de ejemplo
+            calculate.DisplayCamelCaseString(prueba);//se llama la funcion para que trabaje la prueba y realice la funcion para la que fue creada en este caso contar y separar el texto
+            Console.ReadKey();
+        }
+       
+    }
+    public class calculate {
+        public string DisplayCamelCaseString(string camelCase)//metodo publico para que pueda ser accesado por todas las instancias del priyecto
+        {
+            int cont = 1;
+            List<char> caracterMayuscula = new List<char>();//hago una lista de tipo char para que el ciclo pase por cada caracter que haya en la variable
+            caracterMayuscula.Add(camelCase[0]);
+            foreach (char c in camelCase.Skip(1))//el ciclo inicia aqui para que evalue todo el string pero caracter por caracter y si encuentra
+            {
+                if (char.IsUpper(c))//un caracter en mayuscula añada un espacio y que agruegue uno al contador para que se lleve el control de las palabras
+                {
+                    caracterMayuscula.Add(' ');//añade el espacio antes de que continue otra mayuscula
+                    cont=cont + 1;
+                    caracterMayuscula.Add(char.ToUpper(c));
+                }
+                else
+                    caracterMayuscula.Add(c);//si lo anterior no se cumple lo que realizaria es que continue recorriendo el string
+            }
+            Console.WriteLine("La cantidad de palabras es: "+cont);
+            Console.WriteLine("La cadena de palabras es: " + new string(caracterMayuscula.ToArray()));
+            return new string(caracterMayuscula.ToArray());//returna el nuevo string que en este caso seria ya separado por los espacios
+  // escogi este metodo porque me parecio util usar las librerias genericas que trae ya el visual por lo que 
+  //creo que es una solucion optima para dar un resultado favorable ademas es una solucion que busque en internet pero que
+  //supe como es que la cosas pasan dentro del codigo, creo que es optima porque no hay que utilizar librerias ajenas a las que ya vienen
+  //implicitas en c# ademas de ser una solucion sencilla de entender y que da un resultado favorable.
+        }
+
+    }
+} 
